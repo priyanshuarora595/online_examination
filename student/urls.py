98c2f3fdfb9837c2b70e46f2fee5587d5918a25d
Priyanshu Arora <priyanshuarora595@gmail.com>
2024-04-19 18:40:21 +0530

    fix: exam timing issue

--- student/urls.py
+++ student/urls.py
@@ -14,7 +14,8 @@ path('student-dashboard', views.student_dashboard_view,name='student-dashboard')
 
 path('student-exam', views.student_exam_view,name='student-exam'),
 path('take-exam/<int:pk>', views.take_exam_view,name='take-exam'),
-path('start-exam/<int:pk>/<slug:access_code>', views.start_exam_view,name='start-exam'),
+path('verify-exam/<int:pk>/<slug:access_code>', views.verify_exam_view,name='verify-exam'),
+path('start-exam/<int:pk>', views.start_exam_view,name='start-exam'),
 
 path('calculate-marks', views.calculate_marks_view,name='calculate-marks'),
 path('view-result', views.view_result_view,name='view-result'),

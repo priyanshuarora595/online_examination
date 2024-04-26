383f3c100b5e8c017b95e5b3407601559f556480
Priyanshu Arora <priyanshuarora595@gmail.com>
2024-04-26 13:09:24 +0530

    fix: added script for update

--- ci_cd/main.py
+++ ci_cd/main.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-import git
+# import git
 import os
 from django.views.decorators.csrf import csrf_exempt
 
@@ -7,14 +7,14 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def webhook(request):
     if request.method == "POST":
-        repo = git.Repo(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        origin = repo.remotes.origin
-        # os.system("workon online-exam")
-        os.system("source ~/.virtualenvs/online-exam/bin/activate")
-        os.system("git stash -u")
-        origin.pull(os.getenv("MAIN_BRANCH", "main"))
-        os.system("git stash pop")
-        os.system("pip install -r requirements.txt")
+        # repo = git.Repo(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        # origin = repo.remotes.origin
+        os.system("./main_script.sh")
+        # os.system("source ~/.virtualenvs/online-exam/bin/activate")
+        # os.system("git stash -u")
+        # origin.pull(os.getenv("MAIN_BRANCH", "main"))
+        # os.system("git stash pop")
+        # os.system("pip install -r requirements.txt")
         return HttpResponse("Updated Succesfully", status=200)
     else:
         return HttpResponse("Wrong event type", status=400)

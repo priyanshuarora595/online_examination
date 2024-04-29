a1818b2dd7f120a1ce606ae153d3e09f94a24899
Priyanshu Arora <priyanshuarora595@gmail.com>
2024-04-29 13:15:16 +0530

    fix: webhook script location

--- ci_cd/main.py
+++ ci_cd/main.py
@@ -7,14 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def webhook(request):
     if request.method == "POST":
-        # repo = git.Repo(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        # origin = repo.remotes.origin
-        os.system("./main_script.sh")
-        # os.system("source ~/.virtualenvs/online-exam/bin/activate")
-        # os.system("git stash -u")
-        # origin.pull(os.getenv("MAIN_BRANCH", "main"))
-        # os.system("git stash pop")
-        # os.system("pip install -r requirements.txt")
+        os.system("ci_cd/main_script.sh")
         return HttpResponse("Updated Succesfully", status=200)
     else:
         return HttpResponse("Wrong event type", status=400)
